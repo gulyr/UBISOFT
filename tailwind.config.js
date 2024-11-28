1f20153f4b1dff4c4bb9d76558bbcfac723c2d4f
Gul Rasulzada <guldastayr@code.edu.az>
2024-11-28 14:57:41 +0400

    refactor: "footer and other sections updated"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       transitionTimingFunction: {
-        'custom-ease': 'cubic-bezier(0.22, 1, 0.36, 1)',
+        "custom-ease": "cubic-bezier(0.22, 1, 0.36, 1)",
       },
       transform: {
-        'custom-matrix': 'matrix(1.5, 0.5, 0.5, 1.5, 20, 20)',
+        "custom-matrix": "matrix(1.5, 0.5, 0.5, 1.5, 20, 20)",
       },
       transform: {
-        'custom-matrix-2': 'matrix(1.5, 0.5, 0.5, 1.5, 20, 20)',
+        "custom-matrix-2": "matrix(1, 0, 0, 1, 0, -7.3125)",
       },
       gridTemplateColumns: {
-        custom1: '664.5px 664.5px',
+        custom1: "664.5px 664.5px",
       },
     },
   },
   plugins: [],
-}
+};
