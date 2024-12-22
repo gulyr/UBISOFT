94ee6880639e70c04fb83508aa1e09cdfbc887c2
gulyr <guldastayr@code.edu.az>
2024-12-23 01:09:54 +0400

    refactor: "Responsive added to  Latest News section'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       transitionTimingFunction: {
-        "custom-ease": "cubic-bezier(0.22, 1, 0.36, 1)",
+        'custom-ease': 'cubic-bezier(0.22, 1, 0.36, 1)',
       },
       transform: {
-        "custom-matrix": "matrix(1.5, 0.5, 0.5, 1.5, 20, 20)",
+        'custom-matrix': 'matrix(1.5, 0.5, 0.5, 1.5, 20, 20)',
       },
       transform: {
-        "custom-matrix-2": "matrix(1, 0, 0, 1, 0, -7.3125)",
+        'custom-matrix-2': 'matrix(1, 0, 0, 1, 0, -7.3125)',
       },
       gridTemplateColumns: {
-        custom1: "664.5px 664.5px",
+        custom1: '664.5px 664.5px',
       },
       fontFamily: {
-        "ubisoft-bold": ["Ubisoft Sans Bold", "sans-serif"],
+        'ubisoft-bold': ['Ubisoft Sans Bold', 'sans-serif'],
+      },
+      screens: {
+        'exact-1024': { raw: '(min-width: 1024px) and (max-width: 1024px)' }, // Tam olaraq 1024px
       },
     },
   },
   plugins: [],
-};
+}
